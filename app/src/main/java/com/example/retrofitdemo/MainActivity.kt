7f3c341a6821ada2db85e749c4df2c54cd94b6e4
RC <rckashyapcpe@gmail.com>
2023-07-28 13:36:45 +0800

    created a retrofit call

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val BASE_URL = "https://jsonplaceholder.typicode.com/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getAllComments()
+    }
+    private fun getAllComments() {
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MyApi::class.java)
+
+        api.getComments().enqueue(object: Callback<List<Comments>>{
+            override fun onResponse(
+                call: Call<List<Comments>>,
+                response: Response<List<Comments>>
+            ) {
+                if (response.isSuccessful) {
+                    for (data in response.body()!!) {
+                        Log.d("MYDATA", data.name)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Comments>>, t: Throwable) {
+                Log.d("MYDATA", "ERROR")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/retrofitdemo/MyApi.kt
@@ -0,0 +1,9 @@
+package com.example.retrofitdemo
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MyApi {
+    @GET("comments")
+    fun getComments(): Call<List<Comments>>
+}
